be0369fbab70249bf3dc66b19cfb04e983d1be7f
milky245 <xuezhihan@toki.waseda.jp>
2026-04-13 11:16:47 +0900

    Add lesson1 Java project

---
+++ SourceCode/lesson1/src/day01/Lesson1.java
@@ -0,0 +1,37 @@
+//問題
+//パッケージ名 day1
+//クラス名 Lesson1.java
+
+//"出力します"とコンソールに出力してください。
+//続けて12、 1.6、 "こんにちは"、 'a'をそれぞれ変数に代入しコンソールに出力してください。
+//また1行コメントで記入しなさい。（コメント例 〇〇を出力する）
+
+//【出力例】
+//出力します
+//12
+//1.6
+//こんにちは
+//a
+
+package day01;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		//ここから記述
+		System.out.println("出力します");
+		System.out.println(12);
+		System.out.println(1.6);
+		System.out.println("こんにちは");
+		System.out.println("a");
+		//Lesson1 のテキストを出力する
+
+		int num1 = 12;
+		double fl1 = 1.6;
+		String str1 = "こんにちは";
+		char ch1 = 'a';
+		System.out.printf("%d%n%.1f%n%s%n%c%n", num1, fl1, str1, ch1);
+		//Another way to output
+	}
+
+}
---
+++ SourceCode/lesson1/src/lesson03/Sample0303.java
@@ -0,0 +1,11 @@
+package lesson03;
+
+public class Sample0303 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, World!");
+		System.out.println("ようこそ、ジャバへ");
+		System.out.println("Using JDK 17 now");
+	}
+}
